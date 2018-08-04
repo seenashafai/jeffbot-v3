b1f4dcd8208b91e8ccf6494de5a922ac5b94214a
seenashafai <seenas@btinternet.com>
2018-08-04 15:19:34 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+//Calling Discord.js Package
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require('./config.json')
+
+
+//Listener Event: Message Received
+bot.on('message', message =>  {
+
+  //Variables
+  var sender = message.author;
+  var msg = message.content.toUpperCase();
+  var prefix = ">"
+
+  //Ping Pong
+  if (msg === prefix + 'Ping')
+  {
+    message.channel.send('Pong!')
+  }
+});
+
+bot.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  bot.user.setActivity(`Serving ${bot.guilds.size} JeffLords`);
+});
+
+//Login
+bot.login(config.token);
